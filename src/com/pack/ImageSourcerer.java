21965b1f172a55c3bf9bfd879dc2322dfcc00d95
B-C-E <syanik1236@student.whatcom.edu>
2019-12-22 15:55:42 -0800

    Started code outline.

---
+++ src/com/pack/ImageSourcerer.java
@@ -0,0 +1,20 @@
+package com.pack;
+
+//This class is used to take a folder full of pngs and convert them into some nice binary grids.
+//to be used with ImageBook and InkedGrid
+public class ImageSourcerer
+{
+
+    public ImageSourcerer()
+    {}
+
+    //Put your images in a file called ImportImages in your downloads folder
+    //please specify the dimensions of your desired images
+    public ImageBook bookFromFolder(int width, int height)
+    {
+        //make our new ImageBook
+        //get a list of all the files
+        //for each, turn them into binary
+
+    }
+}
