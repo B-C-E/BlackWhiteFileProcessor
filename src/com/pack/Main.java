65b8724f28ce50f7385d1098e4e76450343f582b
B-C-E <syanik1236@student.whatcom.edu>
2019-12-22 19:37:58 -0800

    Adjusted Ink Allowance. 180 seems to be a good amount.

--- src/com/pack/Main.java
+++ src/com/pack/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("");
 	ImageBook book = new ImageBook(256,256);
-	book.loadImages(1,20);
+	book.loadImages(1,180);
 	book.saveToTxt("SavedImagesAsText");
 
 	for(int i = 0; i < book.gridList.size(); i++)
